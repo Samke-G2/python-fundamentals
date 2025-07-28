25bb28ea9ac6f1d692dc384b5a6fb307211d9c79
Samkelwa <samkelwa101@gmail.com>
2025-07-28 23:13:46 +0200

    finished the nautical miles program, and started on the weekly wage one.

--- chapter2/nautical-miles.py
+++ chapter2/nautical-miles.py
@@ -13,3 +13,11 @@ This program converts kilometers to nautical miles.
 3. outputs :
     - The nautivcal miles equivalent of the km
 """
+print("\nThis program converts km to nautical miles. \n")
+
+km = input("How many kilometers would you like to convert: ")
+km = float(km)
+
+nautical_miles = 1.8518518518518519 * km
+
+print(f"\n{km} km is equal to: {nautical_miles} nautical miles\n")
---
+++ chapter2/weekly-wage.py
@@ -0,0 +1,6 @@
+"""
+weekly-wage.py
+author: Samke_g2
+
+This program calculates an employee's total weekly pay based on the total regular hours worked and overtime hours worked.
+"""
