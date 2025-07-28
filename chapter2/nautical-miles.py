f892a1bdcdc91dadc3d4786169f23ed31aff0b40
Samkelwa <samkelwa101@gmail.com>
2025-07-28 22:43:39 +0200

    I started a project that coverts km to nautical miles, no code yet, just the docstring so far.

---
+++ chapter2/nautical-miles.py
@@ -0,0 +1,15 @@
+"""
+nautical-miles.py
+author: Samke_g2
+
+This program converts kilometers to nautical miles.
+
+1. Important constants :
+    - 1km = 0.54 nautical miles (according to my calculations, subject to better info)
+
+2. inputs :
+    - The value of km to be converted
+
+3. outputs :
+    - The nautivcal miles equivalent of the km
+"""
